ecd1b79022bc93e82072586b6a023c47fa80b83c
Tom Stickel <tazmanrising@gmail.com>
2015-04-27 12:59:04 -0700

    moving to new area  initial placeholder on new different github account

---
+++ SilverlightBingMaps/Customer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Maps.MapControl;
+
+namespace SilverlightBingMaps
+{
+    public class Customer : Pushpin
+    {
+        public string CustomerName { get; set; }
+        public string ComputerType { get; set; }
+
+        public Customer(string name, string type, double lat, double lon)
+        {
+            CustomerName = name;
+            ComputerType = type;
+            Location = new Location(lat, lon);
+            ToolTipService.SetToolTip(this, name);
+            if (type == "Mac")
+            {
+                Background = new SolidColorBrush(Colors.Blue);
+                
+            }
+            else
+            {
+                Background = new SolidColorBrush(Colors.Green);
+            }
+        }
+    }
+}
---
+++ SilverlightBingMaps/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Maps.MapControl;
+
+namespace SilverlightBingMaps
+{
+    public partial class MainPage : UserControl
+    {
+        public List<Customer> Customers { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+            //http://blog.credera.com/topic/technology-solutions/business-intelligence/building-interactive-maps-with-silverlight-and-bing-maps-2/
+
+            MyMap.Center = new Location(39.226, -99.829);
+            MyMap.ZoomLevel = 5;
+
+            Customers = new List<Customer>();
+            Customers.Add(new Customer("Bill", "PC", 37, -79));
+            Customers.Add(new Customer("Steve", "Mac", 30, -90));
+            Customers.Add(new Customer("Maria", "PC", 45, -92));
+            Customers.Add(new Customer("Andy", "Mac", 45, -108));
+            Customers.Add(new Customer("Tiffany", "PC", 40, -118));
+            Customers.Add(new Customer("Ryan", "PC", 35, -102));
+
+
+        }
+
+        private void btnLoadCustomers_Click(object sender, RoutedEventArgs e)
+        {
+            MyMap.Children.Clear();
+            ComboBoxItem item = cbCompType.SelectedItem as ComboBoxItem;
+            string compType = item.Content.ToString();
+
+            foreach (Customer cust in Customers)
+            {
+                if (cust.ComputerType == compType)
+                    MyMap.Children.Add(cust);
+                else if (compType == "-All-")
+                    MyMap.Children.Add(cust);
+            }
+
+        }
+    }
+}
